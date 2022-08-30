70616839e6651957bc6628894f59429e488f9198
Ruslan Yusupov <ruslan.yusupov@midhard.com>
2022-08-30 15:54:16 +0300

    harvestable prefabs

--- Assets/Scripts/Gameplay/Harvestable.cs
+++ Assets/Scripts/Gameplay/Harvestable.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Fusion;
 using Fusion.KCC;
 using RPGGame.Model;
+using TMPro;
 using UnityEngine;
 
 namespace RPGGame.Gameplay
@@ -11,6 +12,13 @@ namespace RPGGame.Gameplay
     {
         [SerializeField] private InventoryItemConfig _harvestItem;
         public InventoryItemConfig HarvestItem => _harvestItem;
+
+        [SerializeField] private TMP_Text _label;
+
+        private void Awake()
+        {
+            _label.text = _harvestItem.Name;
+        }
     }
 }
 
